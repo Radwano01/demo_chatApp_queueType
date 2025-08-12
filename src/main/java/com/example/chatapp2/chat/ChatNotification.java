a10207a50fceb66440aa36b32a10042041b55987
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2025-08-13 00:19:09 +0300

    Add files via upload

---
+++ src/main/java/com/example/chatapp2/chat/ChatNotification.java
@@ -0,0 +1,15 @@
+package com.example.chatapp2.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatNotification {
+    private String id;
+    private String senderId;
+    private String recipientId;
+    private String content;
+}
---
+++ src/main/java/com/example/chatapp2/user/Status.java
@@ -0,0 +1,5 @@
+package com.example.chatapp2.user;
+
+public enum Status {
+    OFFLINE, ONLINE;
+}
